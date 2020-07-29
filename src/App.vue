774faadb6a3d3eb6ef4d8c1b4adf94711eaaf2ca
Longraf <13longraf@mail.ru>
2020-07-29 19:22:39 +0300

    something learn

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,65 @@
 <template>
-  <div id="app">
+  <div id="vue-app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>
+      <p>{{count}}</p>
+      <button class="btn__default" @click="changeCount('+10')">+10</button>
+      <button class="btn__default" @click="changeCount('+')">+</button>
+      <button class="btn__default" v-on:click="changeCount('-')">-</button>
+      <button class="btn__default" v-on:click="changeCount('-10')">-10</button>
+    </h1>
+    <h1 v-on:click.once="changeName()">{{text}} !!</h1>
+    <input type="text">
+    <Caption caption="123"/>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Caption from "./components/Caption";
 
 export default {
-  name: 'App',
+  name: 'MyApp',
+  data (){
+    return {
+      count: "11",
+      text: "lalala"
+    }
+  },
+  computed:{
+  },
+  methods:{
+    changeCount(act){
+      if (act == '-'){
+        return this.count--
+      } else if (act == '+') {
+        return this.count++
+      } else if (act == '+10'){
+        return this.count = +this.count + 10
+      } else if (act == '-10'){
+        return this.count -= 10
+      }
+      this.count < 0 ? this.count = 0 : ''
+      if (this.count < 0){
+        this.count = 0
+      }
+    },
+    changeName(){
+      let newText = 'trololo';
+      console.log(this.text);
+
+      this.text = `Вы изменили ${this.text} на ${newText}`
+      setTimeout(()=>{
+        this.text = newText;
+      }, 1000)
+    },
+  },
   components: {
-    HelloWorld
+    Caption
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Caption.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1 >
+        <p>{{caption}}</p>
+    </h1>
+</template>
+
+<script>
+    export default {
+        name: 'Caption',
+        props: {
+            caption: String
+        }
+    }
+</script>
