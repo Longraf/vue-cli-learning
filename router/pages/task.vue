e43ebcc40226cc2ff8f99ec963e408aff3e66f77
LasVegas <dodaaa@bk.ru>
2020-08-09 12:12:29 +0300

    Поправил страницу таска.

--- router/pages/task.vue
+++ router/pages/task.vue
@@ -1,17 +1,13 @@
 <template>
   <div class="vue-app__inputs-wrapper">
     <div class="vue-app__input-value">
-      <!--        <p>{{newTask.task}}</p>-->
       <div class="vue-app__input-value-wrapper">
-        <input class="input input--w300" placeholder="Введите задачу" type="text" v-model="task"
-               :class="taskError ? 'input__red' : '' ">
-        <input class="input input--w300" placeholder="Описание" type="text" v-model="desc">
-        <input class="input input--w300" placeholder="Задайте период выполнения" type="text" v-model="executePeriod">
-        <input @click="this.goToDoList" class="btn btn__submit btn__submit--w200" type="submit" value="Вернуться к списку дел">
+        <input class="input input--w300" placeholder="Введите задачу" type="text" v-model="task.task"
+               :class="task.length <= 0 ? 'input__red' : '' ">
+        <input class="input input--w300" placeholder="Описание" type="text" v-model="task.desc">
+        <input class="input input--w300" placeholder="Задайте период выполнения" type="text" v-model="task.executePeriod">
+        <input @click="$router.push('/todo-list')" class="btn btn__submit btn__submit--w200" type="submit" value="Вернуться к списку дел">
       </div>
-
-
-
     </div>
     <p class="vue-app__change-warning"><b>Внимание</b> изменений данных происходит мгновенно </p>
   </div>
@@ -25,58 +21,21 @@
 
   export default {
     name: 'ChangeTask',
-    data() {
-      return {
-        taskObj: this.taskObj,
-        routeID: this.routeID,
-        taskError: false,
-      }
-    },
-    created() {
-      this.taskObj = this.getTasks.filter(item => item.id === this.$route.params.id)[0];
-      this.routeID = this.$route.params.id;
-    },
     computed: {
       ...mapGetters({
-        getSomeThing: 'getSomeThing',
         getTasks: 'getTasks',
-        getDesc: 'getDesc,',
-        getExecutePeriod: 'getExecutePeriod'
       }),
       task: {
         get() {
-          return this.taskObj.task
+          return this.getTasks.filter(item => item.id === parseInt(this.$route.params.id))[0]
         },
         set(task) {
           this.updateTaskById({
             task: task,
-            currentObj: this.taskObj
+            currentObj: this.getTasks.filter(item => item.id === this.$route.params.id)[0]
           })
         }
       },
-      desc: {
-        get() {
-          return this.taskObj.desc
-        },
-        set(desc) {
-          this.updateDescById({
-            desc: desc,
-            currentObj: this.taskObj
-          })
-        }
-      },
-      executePeriod: {
-        get() {
-          return this.taskObj.executePeriod
-        },
-        set(executePeriod) {
-          this.updateExecutePeriodById({
-            executePeriod: executePeriod,
-            currentObj: this.taskObj
-          })
-        }
-      }
-
     },
     methods: {
       ...mapMutations({
@@ -84,20 +43,6 @@
         updateDescById: 'updateDescById',
         updateExecutePeriodById: 'updateExecutePeriodById',
       }),
-      goToDoList() {
-        this.$router.push({name: 'todo-list'})
-      },
-      addTask() {
-        console.log(this.task);
-        this.updateTaskById({
-          id: this.routeID,
-          task: this.task,
-          desc: this.desc,
-        });
-        this.$router.push({
-          name: 'todo-list',
-        })
-      },
     }
   }
 
